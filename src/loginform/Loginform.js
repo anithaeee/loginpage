b7c77485ed98ab2b7797b1d02b0efa214b3ed638
Nageshwar Reddy Pandem <nageshwar.uideveloper@gmail.com>
2023-10-22 10:37:04 +0530

    22nd oct

--- src/loginform/Loginform.js
+++ src/loginform/Loginform.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import '../loginform/Loginform.css';
 import 'primeflex/primeflex.css';
 const Loginform = () => {
-    const [Inputvalue, setInputvalue] = useState({});
+    const [Inputvalue, setInputvalue] = useState([]);
     const [Username, setUsername] = useState("");
     const [Email, setEmail] = useState("");
+    const [Password,setpassword] = useState("");
+    useEffect(()=>{
+        localStorage.setItem('UserInfo',JSON.stringify(Inputvalue))
+    },[Inputvalue])
+
+    useEffect(()=>{
+        const items = JSON.parse(localStorage.getItem('UserInfo'));
+        console.log(items, 'UserInfo');
+        if(items){
+            setInputvalue(items);
+        }
+    },[])
 
     const submitDetails=()=> {
         let object1 = Inputvalue;
-        object1.push({Email:Username,Email: Email })
+        let object2 = object1.filter(x=>(x.Email===Username && x.Password===Password));
+        if(object2.length===0){
+            object1.push({Email:Username, Password:Password});
+        }
+        else{
+            alert('user already exist');
+        }
         setInputvalue(object1);
     }
   return (
@@ -27,7 +45,7 @@ const Loginform = () => {
             </div>
             <div className='text-field'>
                 <label className='Label'>Password:</label>
-                 <InputText placeholder='Password'></InputText>
+                 <InputText placeholder='Password' onChange={(e)=>{setpassword(e.target.value)}}></InputText>
             </div>
             <div style={{padding:"5px"}}>
                 <Button className='btn' onClick={(e)=>{submitDetails()}}>submit</Button>
